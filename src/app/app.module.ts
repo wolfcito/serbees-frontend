126e9abf20de23271c1c1cf9e389222705714afe
Luis Fernando Ushiña <guffenix@gmail.com>
2022-03-11 05:56:34 -0500

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from '@home/home.component';
 import { ProductoModule } from '@producto/producto.module';
 import { CoreModule } from '@core/core.module';
 import { CookieService } from 'ngx-cookie-service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -20,7 +21,8 @@ import { CookieService } from 'ngx-cookie-service';
     BrowserModule,
     AppRoutingModule,
     ProductoModule,
-    CoreModule
+    CoreModule,
+    NgbModule
   ],
   providers: [CookieService],
     bootstrap: [AppComponent],
