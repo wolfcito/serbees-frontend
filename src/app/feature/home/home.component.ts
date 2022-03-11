210b05c8108b43c6841909635687ae617312ee5c
Luis Fernando Ushiña <guffenix@gmail.com>
2022-03-11 15:50:55 -0500

    mensaje login

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild} from '@angular/core';
 import { StorageService } from '@core/services/storage.service';
 import { Usuario } from '@servicio/shared/model/usuario';
 import { ServicioService } from '@servicio/shared/service/servicio.service';
+
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
+import {NgbAlert} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -15,12 +19,17 @@ export class HomeComponent implements OnInit {
   identificacion = 0;
   status: 'Cargando...' | 'Error' | 'Exitoso' | 'Inicial' = 'Inicial';
 
+  staticAlertClosed = false;
+  successMessage = '';
+  private _success = new Subject<string>();
+
+  finalizarEjecucion
+
+  @ViewChild('staticAlert', {static: false}) staticAlert: NgbAlert;
+  @ViewChild('selfClosingAlert', {static: false}) selfClosingAlert: NgbAlert;
+
   constructor(protected storageService: StorageService, protected loginService: ServicioService) { }
 
-  ngOnInit() {
-    this.usuarioInValido = new Usuario("test1",'1234');
-    this.usuarioValidador = new Usuario("test0",'1234');
-  }
 
   ingresarValido() {
     this.status = 'Cargando...';
@@ -29,7 +38,7 @@ export class HomeComponent implements OnInit {
         this.identificacion = data.valor;
         this.status = 'Exitoso';
         this.storageService.guardar(this.identificacion);
-       
+       this.changeSuccessMessage("Con Experiencia");
       }, response => {
         this.status = 'Error';
         console.log(response.error.mensaje)
@@ -44,12 +53,29 @@ export class HomeComponent implements OnInit {
         this.identificacion = data.valor;
         this.status = 'Exitoso';
         this.storageService.guardar(this.identificacion);
-       
+        this.changeSuccessMessage("Sin Experiencia");
       }, response => {
         this.status = 'Error';
         console.log(response.error.mensaje)
         this.storageService.guardar(1);
       });
   }
+
+  ngOnInit(): void {
+    this.usuarioInValido = new Usuario("test1",'1234');
+    this.usuarioValidador = new Usuario("test0",'1234');
+
+    this.finalizarEjecucion = setTimeout(() => this.staticAlert.close(), 20000);
+
+    this._success.subscribe(message => this.successMessage = message);
+    this._success.pipe(debounceTime(5000)).subscribe(() => {
+      if (this.selfClosingAlert) {
+        this.selfClosingAlert.close();
+      }
+    });
+  }
+
+  public changeSuccessMessage(sms: string) { this._success.next(`Se ha logueado correctamente - Usuario ${sms}`); }
+
 
 }
