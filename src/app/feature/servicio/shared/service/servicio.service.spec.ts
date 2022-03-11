f10ed720906f9bb0f6706f8d98c453439992fedb
Luis Fernando Ushiña <guffenix@gmail.com>
2022-03-11 08:38:46 -0500

    fix test services

--- src/app/feature/servicio/shared/service/servicio.service.spec.ts
+++ src/app/feature/servicio/shared/service/servicio.service.spec.ts
@@ -1,16 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+// import { TestBed } from '@angular/core/testing';
 
 import { ServicioService } from './servicio.service';
 
-describe('ServicioService', () => {
-  let service: ServicioService;
+describe('Servicio HTTP GET/POST', () => {
+  let serviceGet: ServicioService;
+  let servicePost: ServicioService;
+  let httpClientSpyGet: jasmine.SpyObj<HttpClient>;
+  let httpClientSpyPost: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ServicioService);
+    // TestBed.configureTestingModule({});
+    // service = TestBed.inject(ServicioService);
+    httpClientSpyGet = jasmine.createSpyObj('HttpClient', ['get']);
+    serviceGet = new ServicioService(httpClientSpyGet as any);
+
+    httpClientSpyPost = jasmine.createSpyObj('HttpClient', ['post']);
+    servicePost = new ServicioService(httpClientSpyPost as any);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('Deberia crearse el servicio GET correctamente', () => {
+    expect(serviceGet).toBeTruthy();
   });
+
+  it('Deberia crearse el servicio POST correctamente', () => {
+    expect(servicePost).toBeTruthy();
+  });
+
 });
